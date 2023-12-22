9946d3798b305944b4428e9ad01f108712fb4e19
Adrián Casais Vidal <adriancasaisvidal@adrians-macbook-pro.home>
2023-12-22 12:24:50 +0100

    Finish day 3

--- 2023/cpp/3b/main.cpp
+++ 2023/cpp/3b/main.cpp
@@ -9,10 +9,7 @@
 // #include <bits/stdc++.h>
 struct OpenFile {
   OpenFile(std::string name) { m_input.open(name, std::ios::in); }
-  ~OpenFile() {
-    m_input.close();
-    // printf("calling dtor\n");
-  }
+  ~OpenFile() { m_input.close(); }
   std::fstream m_input;
   std::fstream *getInput() { return &m_input; }
 };
@@ -42,17 +39,20 @@ struct NumPos {
   int last;
 };
 
-bool adjacent(NumPos digit, std::vector<int> positions, bool current) {
-  bool adjacent = false;
+std::pair<unsigned, int> adjacent(int star, std::vector<NumPos> positions) {
+  unsigned counter = 0;
+  int gear = 1;
   for (int i = 0; i < positions.size(); i++) {
-    adjacent |= ((digit.first - positions[i]) == 1) ||
-                ((digit.last - positions[i]) == -1);
-    // if (!current)
-    adjacent |= digit.first <= positions[i] && positions[i] <= digit.last;
-    // if (adjacent) std::cout << "digit " << digit.digit << " is adjacent in
-    // pos " << positions[i] << std::endl;
+    bool isadjacent = false;
+    isadjacent |= ((star - positions[i].first) == -1) ||
+                  ((star - positions[i].last) == 1);
+    isadjacent |= star >= positions[i].first && positions[i].last >= star;
+    if (isadjacent) {
+      gear *= std::atoi(positions[i].digit.c_str());
+      counter += 1;
+    }
   }
-  return adjacent;
+  return {counter, gear};
 }
 std::vector<NumPos> tokenize_nums(std::string line) {
 
@@ -78,40 +78,41 @@ std::vector<NumPos> tokenize_nums(std::string line) {
 std::vector<int> get_punct_positions(std::string line) {
   std::vector<int> output;
   for (unsigned i = 0; i < line.size(); i++) {
-    if (std::ispunct(line[i]) && line[i] != *".") {
+    if (line[i] == *"*") {
       output.push_back(i);
     }
   }
   return output;
 }
 int current_line_result(std::string previous_line, std::string current_line,
-                 std::string next_line) {
+                        std::string next_line) {
 
   std::vector<NumPos> next_digits = tokenize_nums(next_line);
-  std::vector<NumPos> current_digits = tokenize_nums(current_line);
   std::vector<NumPos> previous_digits = tokenize_nums(previous_line);
+  std::vector<NumPos> current_digits = tokenize_nums(current_line);
 
-  std::vector<int> current_pos = get_punct_positions(current_line);
-  std::vector<int> next_pos = get_punct_positions(next_line);
-  std::vector<int> previous_pos = get_punct_positions(previous_line);
-  bool isadjacent = false;
+  std::vector<int> current_stars = get_punct_positions(current_line);
   int current_result = 0;
-  for (auto digit : current_digits) {
-    isadjacent |= adjacent(digit, current_pos, true);
-    isadjacent |= adjacent(digit, next_pos, false);
-    isadjacent |= adjacent(digit, previous_pos, false);
-    if (isadjacent) {
-      int appender = myatoi(digit.digit);
-      current_result += appender;
-      isadjacent = false;
-    }
+  for (auto star : current_stars) {
+    // std::cout << "Star in " << star << std::endl;
+    auto [prev_adj, prev_gear] = adjacent(star, previous_digits);
+    auto [next_adj, next_gear] = adjacent(star, next_digits);
+    auto [this_adj, this_gear] = adjacent(star, current_digits);
+    if (prev_adj + next_adj == 2)
+      current_result += prev_gear * next_gear;
+    else if (prev_adj + this_adj == 2)
+      current_result += prev_gear * this_gear;
+    else if (next_adj + this_adj == 2)
+      current_result += next_gear * this_gear;
+    else if (this_adj == 2)
+      current_result += this_gear;
   };
   return current_result;
 }
 int main() {
   auto start = std::chrono::high_resolution_clock::now();
   OpenFile myfile("input.txt");
-  auto *p = myfile.getInput();
+  auto *input_pointer = myfile.getInput();
   std::string tp;
   int final_result = 0;
   bool lastline = false;
@@ -126,9 +127,9 @@ int main() {
   std::vector<int> current_pos;
   std::vector<int> next_pos;
   std::vector<int> previous_pos;
-  std::getline(*p, current_line);
+  std::getline(*input_pointer, current_line);
   unsigned line_counter = 1;
-  while (std::getline(*p, next_line)) {
+  while (std::getline(*input_pointer, next_line)) {
     final_result += current_line_result(previous_line, current_line, next_line);
     previous_line = current_line;
     current_line = next_line;
